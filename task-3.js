f9144e4786616ef39e5aeabcc7b17b2e62b5d5f1
artemlitun <artemlitun@gmail.com>
2021-02-23 14:11:39 +0200

    Исправил ошибки

--- task-3.js
+++ task-3.js
@@ -1,36 +1,38 @@
 class Storage {
-    constructor(items) {
-        this.items = items;
-    }
-    getItems() {
-        return this.items;
-    }
-    addItem(item) {
-        this.items = [...items, item];
-    }
-    removeItem(item) {
+  constructor(arr) {
+    this.items = arr;
+  }
 
-        if (this.items.includes(item)) {
-            this.items.splice(this.items.indexOf(item), 1 )
-            return this.items;
-        }; 
-    }
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    this.items.includes(item)
+      ? this.items.splice(this.items.indexOf(item), 1)
+      : "";
+  }
 }
-  const storage = new Storage([
-    'Нанитоиды',
-    'Пролонгер',
-    'Железные жупи',
-    'Антигравитатор',
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
 ]);
 
 const items = storage.getItems();
-  
-  console.table(items); 
-  
-  storage.addItem('Дроид');
-  console.table(storage.items); 
-  
-storage.removeItem('Пролонгер');
-storage.removeItem('Железные жупи');
-  
-  console.table(storage.items);
+
+console.table(items);
+
+storage.addItem("Дроид");
+console.table(storage.items);
+
+storage.removeItem("Пролонгер");
+storage.removeItem("Железные жупи");
+
+console.table(storage.items);
--- task-5.js
+++ task-5.js
@@ -1,70 +1,78 @@
 class Car {
-  constructor({ maxSpeed, price }, speed = 0, isOn = false, distance = 0) {
-      this.maxSpeed = maxSpeed;
-      this.speed = speed;
-      this.isOn = isOn;
-      this.distance = distance;
-      this.price = price;
-    }
-  
-    static getSpecs(car) {
-      console.log(car);
-    }
-  
-    get newPrice() {
-      return this.price;
-    }
-  
-    set newPrice(value) {
-      this.price = value;
-    }
-  
-    turnOn() {
-      this.isOn = true;
-    }
-  
-    turnOff() {
+  constructor(obj, speed = 0, isOn = false, distance = 0) {
+    this.maxSpeed = obj.maxSpeed;
+    this.newPrice = obj.price;
+    this.speed = speed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed = ${car.maxSpeed}, speed = ${car.speed}, isOn = ${car.isOn}, distance = ${car.distance}, price = ${car.price}`
+    );
+  }
+
+  get price() {
+    return this.newPrice;
+  }
+
+  set price(amount) {
+    this.newPrice = Number(amount);
+  }
+
+  turnOn() {
+    this.isOn === false ? (this.isOn = true) : console.log("car is already On");
+  }
+
+  turnOff() {
+    if (this.isOn === true) {
       this.isOn = false;
       this.speed = 0;
-    }
-  
-    
-    accelerate(value) {
-      if (this.speed + value < this.maxSpeed) {
-        this.speed += value;
-      }
-    }
-  
-    decelerate(value) {
-      if (this.speed - value > 0) {
-        this.speed = this.speed - value;
-      }
-    } 
-  
-    drive(hours) {
-      if ((this.isOn)) {
-        this.distance += hours * this.speed;
-      }
+    } else {
+      console.log("car is already Off");
     }
   }
-  
-  const mustang = new Car({ maxSpeed: 200, price: 2000 });
-  
-  mustang.turnOn();
-  mustang.accelerate(50);
-  mustang.drive(2);
-  
-  Car.getSpecs(mustang);
-  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
-  
-  mustang.decelerate(20);
-  mustang.drive(1);
-  mustang.turnOff();
-  
-  Car.getSpecs(mustang);
-  // maxSpeed: 200, speed: 30, isOn: false, distance: 130, price: 2000
-  
+
+  accelerate(value) {
+    if (this.speed + value > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    } else {
+      this.speed += Number(value);
+    }
+  }
+
+  decelerate(value) {
+    if (this.speed - value < 0) {
+      this.speed = 0;
+    } else {
+      this.speed -= Number(value);
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += Number(hours) * this.speed;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 30, isOn: false, distance: 130, price: 2000
+
 console.log(mustang.price); // 2000
-  
+
 mustang.price = 4000;
 console.log(mustang.price); // 4000
